9653a2d8216d9603d631b0ae80edd4a634b65b77
georgiv5 <146932875+georgiv5@users.noreply.github.com>
2024-01-03 23:17:46 +0000

    Data

--- app_sec/php/logout.php
+++ app_sec/php/logout.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 // Initialize the session
 session_start();
@@ -17,3 +18,64 @@ syslog(LOG_INFO, $timestamp + " Session terminated\n");
 header("location: login.php");
 exit;
 ?>
+=======
+<<<<<<< Updated upstream
+<?php
+// Initialize the session
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session.
+session_destroy();
+
+// Expire the session cookie by setting its expiration in the past
+setcookie('session_token', '', time() - 3600, '/', '', true, true);
+
+// Clear client-side data using JavaScript
+echo '<script>';
+echo 'localStorage.clear();'; // Clear local storage
+echo 'sessionStorage.clear();'; // Clear session storage
+echo '</script>';
+
+$t = time();
+$timestamp = date("Y-m-d", $t);
+
+syslog(LOG_INFO, $timestamp + " Session terminated\n");
+
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
+=======
+<?php
+// Initialize the session
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session.
+session_destroy();
+
+// Expire the session cookie by setting its expiration in the past
+setcookie('session_token', '', time() - 3600, '/', '', true, true);
+
+// Clear client-side data using JavaScript
+echo '<script>';
+echo 'localStorage.clear();'; // Clear local storage
+echo 'sessionStorage.clear();'; // Clear session storage
+echo '</script>';
+
+$t = time();
+$timestamp = date("Y-m-d", $t);
+
+syslog(LOG_INFO, $timestamp + " Session terminated\n");
+
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
