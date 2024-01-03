9546edb6c86da91bdc97d3c6dc47e2e90b3d00da
georgiv5 <146932875+georgiv5@users.noreply.github.com>
2024-01-03 23:58:09 +0000

    Update logout.php

--- app_sec/php/logout.php
+++ app_sec/php/logout.php
@@ -5,22 +5,16 @@ session_start();
 // Unset all of the session variables
 $_SESSION = array();
 
-// Destroy the session.
+// Destroy the session
 session_destroy();
 
 // Expire the session cookie by setting its expiration in the past
 setcookie('session_token', '', time() - 3600, '/', '', true, true);
 
-// Clear client-side data using JavaScript
-echo '<script>';
-echo 'localStorage.clear();'; // Clear local storage
-echo 'sessionStorage.clear();'; // Clear session storage
-echo '</script>';
-
 $t = time();
 $timestamp = date("Y-m-d", $t);
 
-syslog(LOG_INFO, $timestamp + " Session terminated\n");
+syslog(LOG_INFO, $timestamp . " Session terminated\n");
 
 // Redirect to login page
 header("location: login.php");
